a04bb872a46e9a7948c4397e744ffccabd35615a
Konrad Borowski <xfix@protonmail.com>
2016-07-10 14:10:58 +0200

    Introduce some unit tests

---
+++ html2irc_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+var tests = []struct {
+	in  string
+	out []string
+}{
+	{"a", []string{"a"}},
+	{"<div>a</div><div>b</div>", []string{"a", "b"}},
+	{"<div><div>a</div>b<span>c</span><div>d</div></div>e", []string{"a", "bc", "d", "e"}},
+}
+
+func TestHTMLToIRC(t *testing.T) {
+	for _, test := range tests {
+		out := htmlToIRC(test.in)
+		if !reflect.DeepEqual(out, test.out) {
+			t.Errorf("htmlToIRC(%q) => %q, want %q", test.in, out, test.out)
+		}
+	}
+}
