9253c918895e89efd4e9baa207f12d46bed0f84d
Konrad Borowski <xfix@protonmail.com>
2016-07-18 23:56:32 +0200

    Add test for GetMessage in numerics

---
+++ irc/numerics_test.go
@@ -0,0 +1,12 @@
+package irc
+
+import "testing"
+
+func TestGetMessage(t *testing.T) {
+	input := ErrNoSuchServer
+	message := input.GetMessage()
+	expected := "%s :No such server"
+	if message != expected {
+		t.Errorf("%q.GetMessage() => %q, want %q", input, message, expected)
+	}
+}
